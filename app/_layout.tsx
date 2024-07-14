d8be3798f1c13a3f2c5451bec51ebc7d5ac78d68
marcosviana91 <marcos.viana.91@gmail.com>
2024-07-14 14:14:47 -0300

    fix - Add a little underlayer to notification bar in android devices

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
+import { Platform } from "react-native";
 import { Provider } from "react-redux";
 import { Slot } from "expo-router";
 
 import { store } from '@/store';
+import { ThemedView } from "@/components/themed/ThemedView";
 
 
 export default function RootLayout() {
     return (
         <Provider store={store}>
+            {Platform.OS === 'android' && <ThemedView style={{ height: 50 }} lightColor="#fffc" darkColor="#000c" />}
             <Slot />
         </Provider>
     );
