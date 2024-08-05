9e46bcb02ca27efe0d5ec43a7881d20a5d844f31
marcosviana91 <marcos.viana.91@gmail.com>
2024-08-04 23:56:25 -0300

    style - bottom pad

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,21 @@
-import { Platform } from "react-native";
+import { Platform, View } from "react-native";
 import { Provider } from "react-redux";
 import { Slot } from "expo-router";
 
 import { store } from '@/store';
 import { ThemedView } from "@/components/themed/ThemedView";
+import { useScreenSizes } from "@/hooks/useScreenSizes";
 
 
 export default function RootLayout() {
+    const { width: windowWidth, height: windowHeight } = useScreenSizes();
     return (
         <Provider store={store}>
             {Platform.OS === 'android' && <ThemedView style={{ height: 50 }} lightColor="#fffc" darkColor="#000c" />}
-            <Slot />
+            <View style={{ height: windowHeight, maxWidth: windowWidth}}>
+                <Slot />
+                {Platform.OS === 'android' && <ThemedView style={{ height: 50 }} lightColor="#fffc" darkColor="#000c" />}
+            </View>
         </Provider>
     );
 }
