bd489f4dabd764afad8fc7f7e6bb86c56d59a0a6
marcosviana91 <marcos.viana.91@gmail.com>
2024-08-08 16:45:32 -0300

    refactor - OnInvokeDefaultAction OK

--- components/cards/cardsComands/heros.tsx
+++ components/cards/cardsComands/heros.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
+import { useSelector } from "react-redux";
 import { Pressable, View, Text } from "react-native"
+
+import { RootReducer } from "@/store";
+
 import useWebSocket from 'react-use-websocket';
 import { URI } from "@/store/server_urls";
 
@@ -7,14 +11,13 @@ import { URI } from "@/store/server_urls";
 import { OnInvoke as EliasOnInvoke } from '@/components/cards/cardsComands/elias';
 import { OnInvoke as EsterOnInvoke } from '@/components/cards/cardsComands/ester';
 import { OnInvoke as MariaOnInvoke } from '@/components/cards/cardsComands/maria';
+
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { ThemedText } from "@/components/themed/ThemedText";
 import { ThemedView } from "@/components/themed/ThemedView";
-import { useSelector } from "react-redux";
-import { RootReducer } from "@/store";
+import { WebSocketHook } from "react-use-websocket/dist/lib/types";
 
 type OnInvokeProps = {
-    onPress?: () => void
     card: CardProps
 }
 
@@ -50,40 +53,36 @@ export function OnInvoke(props: OnInvokeProps) {
                     {render}
                 </>
             }
-            <ThemedView style={{ borderRadius: 8, borderWidth:2, height: "auto", width: "auto", marginVertical:24}}>
-
-                <Pressable
-                    onPress={() => {
-                        if (CARDS_ONINVOKE_LIST.find((nome) => nome == props.card.slug) || (props.card.card_type == 'miracles')) {
-                            setCustomInvoke(true)
-                        } else {
-                            WS.sendJsonMessage({
-                                "data_type": "match_move",
-                                "user_data": {
-                                    "id": player?.id
-                                },
-                                "room_data": {
-                                    "id": matchData?.id
-                                },
-                                "match_move": {
-                                    "match_id": matchData?.id,
-                                    "round_match": matchData?.round_match,
-                                    "player_move": player?.id,
-                                    "card_id": props.card.in_game_id,
-                                    "move_type": "move_to_prepare"
-                                }
-                            })
-                            if (props.onPress) { props.onPress() }
-                        }
-                    }}
-                >
-                    <ThemedText style={{ lineHeight: 80 }}>
-                        <MaterialCommunityIcons name="arrow-up" size={80} />
-
-                    </ThemedText>
-                </Pressable>
+            <ThemedView style={{ borderRadius: 8, borderWidth: 2, height: "auto", width: "auto", marginVertical: 24 }}>
+                <ThemedText style={{ lineHeight: 80 }}>
+                    <MaterialCommunityIcons name="arrow-up" size={80} />
+                </ThemedText>
             </ThemedView>
-
         </>
     )
+}
+
+export function OnInvokeDefaultAction(props: { card: CardProps, matchData: MatchApiProps, player: PlayersInMatchApiProps, web_socket: WebSocketHook<unknown, MessageEvent<any> | null> }) {
+    console.log("CHAMANDO OnInvokeDefaultAction")
+
+    // if (CARDS_ONINVOKE_LIST.find((nome) => nome == props.card.slug)) {
+    //     // setCustomInvoke(true)
+    // } else {
+        props.web_socket.sendJsonMessage({
+            "data_type": "match_move",
+            "user_data": {
+                "id": props.player?.id
+            },
+            "room_data": {
+                "id": props.matchData?.id
+            },
+            "match_move": {
+                "match_id": props.matchData?.id,
+                "round_match": props.matchData?.round_match,
+                "player_move": props.player?.id,
+                "card_id": props.card.in_game_id,
+                "move_type": "move_to_prepare"
+            }
+        })
+    // }
 }
--- components/cards/containers/HandContainer.tsx
+++ components/cards/containers/HandContainer.tsx
@@ -1,22 +1,35 @@
 import { ThemedText } from "@/components/themed/ThemedText"
 import { RootReducer } from "@/store"
-import { Pressable, View, Text } from "react-native"
-import { useDispatch, useSelector } from "react-redux"
+import { View } from "react-native"
+import { useSelector } from "react-redux"
 import DefaultContainer from "./DefaultContainer"
-import { MaterialCommunityIcons } from "@expo/vector-icons"
-import { useState } from "react"
-import { ThemedView } from "@/components/themed/ThemedView"
+import { useEffect, useState } from "react"
 import useWebSocket from "react-use-websocket"
 import { URI } from "@/store/server_urls"
-import { getCardInList } from "@/hooks/useCards"
-import { OnInvoke as HeroOnInvoke } from "../cardsComands/heros"
+import { OnInvoke as HeroOnInvoke, OnInvokeDefaultAction } from "../cardsComands/heros"
 import { OnInvoke as MiracleOnInvoke } from "../cardsComands/miracles"
 
 
 export default function HandContainer() {
     const player = useSelector((state: RootReducer) => state.matchReducer.player_data)!
-    const [actualCardInGameId, setActualCardInGameId] = useState('TESTE')
+    const [selectedCard, setSelectedCard] = useState<CardProps>()
+    const matchData = useSelector((state: RootReducer) => state.matchReducer.match_data)
+    const WS = useWebSocket(`ws://${URI}/ws/`, { share: true });
 
+    function actionFunction(props: { card: CardProps, action_index: number }) {
+
+        switch (props!.action_index) {
+            default:
+                console.log('action_index ', props!.action_index)
+                OnInvokeDefaultAction({
+                    card: props.card,
+                    web_socket: WS,
+                    player: player!,
+                    matchData: matchData!,
+                })
+                break
+        }
+    }
     return (
         <View style={{ borderColor: 'green', borderWidth: 1, height: 100, borderRadius: 8, alignItems: 'center', justifyContent: 'center' }}>
             {player!.card_hand!.length < 1 ?
@@ -24,18 +37,16 @@ export default function HandContainer() {
                 <DefaultContainer
                     card_size="small"
                     cards={player!.card_hand!}
-                    card_action={<OnInvoke in_game_id={actualCardInGameId} />}
-                    get_selected_card={(card_index) => {
-                        const card_in_game_id = player!.card_hand![card_index].in_game_id!
-                        setActualCardInGameId(card_in_game_id)
-                    }}
+                    card_action_component={[<OnInvoke card={selectedCard!} />]}
+                    card_action_function={actionFunction}
+                    get_selected_card={setSelectedCard}
                 />
             }
         </View>
     )
 }
 
-function OnInvoke(props: { in_game_id?: string }) {
+function OnInvoke(props: { card: CardProps }) {
     const matchData = useSelector((state: RootReducer) => state.matchReducer.match_data)!
     const player = useSelector((state: RootReducer) => state.matchReducer.player_data)!
 
@@ -43,16 +54,14 @@ function OnInvoke(props: { in_game_id?: string }) {
     //     return null
     // }
 
-    const card = getCardInList(props.in_game_id, player.card_hand)!
-
-    switch (card.card_type) {
+    switch (props.card.card_type) {
         case "hero":
             return (
-                <HeroOnInvoke card={card} />
+                <HeroOnInvoke card={props.card} />
             )
         case "miracle":
-            return (
-                <MiracleOnInvoke card={card} />
+            return null; (
+                <MiracleOnInvoke card={props.card} />
             )
     }
 
