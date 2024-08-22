a0136db4b933eb255ba21ddde2e23b2d15620ffa
marcosviana91 <marcos.viana.91@gmail.com>
2024-08-22 16:24:44 -0300

    update - API urls

--- store/server_urls.js
+++ store/server_urls.js
@@ -1,6 +1,6 @@
-const API_URL_WWW = 'marcosvianadev2.ddns.net:3110'
+const API_URL_WWW_PROD = 'marcosvianadev2.ddns.net:3110'
+const API_URL_WWW_DEV = 'marcosvianadev2.ddns.net:8000'
 const API_URL_LOCAL_DOCKER = '192.168.1.32:3110'
-const API_URL_LOCAL_PROD = '192.168.1.32:8000'
-const API_URL_LOCAL_DEV = '127.0.0.1:8000'
+const API_URL_LOCAL_DEV = '192.168.1.32:8000'
 
-export const URI = API_URL_WWW
+export const URI = API_URL_LOCAL_DEV
