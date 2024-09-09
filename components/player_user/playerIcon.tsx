1722e44a5706039320393713ede80eb8a1baad54
marcosviana91 <marcos.viana.91@gmail.com>
2024-09-09 12:54:12 -0300

    bug - show players icons

--- components/player_user/playerIcon.tsx
+++ components/player_user/playerIcon.tsx
@@ -5,14 +5,14 @@ import { RootReducer } from '@/store';
 import { setPlayerFocus, addPlayersData } from "@/store/reducers/matchReducer";
 import { useScreenSizes } from "@/hooks/useScreenSizes";
 
-import { View, Image, Text, StyleSheet, Pressable, ScrollView } from "react-native";
+import { View, Image, Text, StyleSheet, Pressable, ScrollView, Dimensions } from "react-native";
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { useGetUserDataMutation } from '@/store/api'
-import { useAvatar } from "@/hooks/usePlayerData";
+import { useAvatar, usePlayerData } from "@/hooks/usePlayerData";
 import { ThemedText } from "../themed/ThemedText";
 import { ThemedView } from "../themed/ThemedView";
 import { TEAM_COLOR } from "@/constants/Colors";
@@ -159,7 +159,8 @@ export function IconsContainer(props: ContainerProps) {
     const playerData = useSelector((state: RootReducer) => state.authReducer.user_data)
     const dispatch = useDispatch();
     const [showPlayers, setShowPlayers] = useState(true)
-    const { width: windowWidth, height: windowHeight } = useScreenSizes();
+    const { width: windowWidth } = useScreenSizes();
+    const window = Dimensions.get('window')
     return (
         <View style={{
             flexDirection: "row",
@@ -172,71 +173,74 @@ export function IconsContainer(props: ContainerProps) {
             <View>
                 <Pressable
                     onPress={() => { setShowPlayers(!showPlayers) }}
+                    style={{ flexDirection: 'row' }}
                 >
                     <FontAwesome5 name={showPlayers ? "users-slash" : "users"} size={32} color="black" />
                 </Pressable>
             </View>
-            {showPlayers &&
-                <ThemedView style={{ maxWidth: windowWidth - 64 }}>
-                    <ScrollView horizontal contentContainerStyle={{
-                        flex: 1,
-                        flexDirection: "row",
-                        columnGap: 8,
-                    }}>
-                        {props.matchData?.players_in_match?.map((_team, _index) => {
-                            return (
-                                <View
+            {showPlayers ?
+                <ThemedView style={{
+                    width: window.width - 60,
+                    maxWidth: windowWidth - 60
+                }}>
+                    {props.matchData?.players_in_match?.map((_team, _index) => {
+                        return (
+                            <View
                                 key={_index}
                                 style={{
                                     flex: 1,
                                     flexDirection: "row",
-                                    columnGap: 16,
+                                    flexWrap: 'wrap',
+                                    justifyContent: 'space-evenly',
+                                    gap: 16,
                                     paddingHorizontal: 8,
                                     borderWidth: 2,
                                     borderColor: TEAM_COLOR[_index],
                                     borderRadius: 12,
                                     padding: 2,
                                 }}>
-                                    {_team.map(_player => {
-                                        return (
-                                            <View key={_player.id} style={{
-                                                height: 100
-                                            }}>
-                                                <Pressable
-                                                    onPress={() => {
-                                                        if (playerData?.id !== _player.id) {
-                                                            dispatch(setPlayerFocus(_player.id))
-                                                        }
-                                                    }}
-                                                    style={{
-                                                        height: '100%',
-                                                    }}
-                                                >
+                                {_team.map(_player => {
+                                    return (
+                                        <View key={_player.id} style={{
+                                            height: 100,
+                                            width: 100,
+                                        }}>
+                                            <Pressable
+                                                onPress={() => {
+                                                    if (playerData?.id !== _player.id) {
+                                                        dispatch(setPlayerFocus(_player.id))
+                                                    }
+                                                }}
+                                                style={{
+                                                    height: '100%',
+                                                }}
+                                            >
+                                                <PlayerIcon id={_player.id} isCurrent={(_player.id == props.matchData!.player_turn)} isTarget={(_player.id == props.matchData!.player_focus_id)} type='mini' />
+                                                <ThemedView style={{ flexDirection: 'row', borderWidth: 1, borderBottomWidth: 0, borderEndWidth: 0, borderStartWidth: 0 }}>
+                                                    <ThemedText>
+                                                        <MaterialCommunityIcons name="shield-cross" size={24} />
+                                                    </ThemedText>
+                                                    <ThemedText type='defaultSemiBold'>{_player.faith_points}</ThemedText>
+                                                    <Image
+                                                        source={require('@/assets/images/Icons/wisdon.png')}
+                                                        style={{ height: 20, width: 20, margin: 4 }}
+                                                    />
+                                                    <ThemedText type='defaultSemiBold'>{_player.wisdom_available}</ThemedText>
+                                                </ThemedView>
+                                            </Pressable>
+                                        </View>
+                                    )
+                                })}
+                            </View>
+                        )
+                        // if (props.hideCurrentPlayer && _player.id === props.player_id) {
+                        //     return null
+                        // }
+                    })}
+                </ThemedView> :
+                <>
 
-                                                    <PlayerIcon id={_player.id} isCurrent={(_player.id == props.matchData!.player_turn)} isTarget={(_player.id == props.matchData!.player_focus_id)} type='mini' />
-                                                    <ThemedView style={{ flexDirection: 'row', borderWidth: 1, borderBottomWidth: 0, borderEndWidth: 0, borderStartWidth: 0 }}>
-                                                        <ThemedText>
-                                                            <MaterialCommunityIcons name="shield-cross" size={24} />
-                                                        </ThemedText>
-                                                        <ThemedText type='defaultSemiBold'>{_player.faith_points}</ThemedText>
-                                                        <Image
-                                                            source={require('@/assets/images/Icons/wisdon.png')}
-                                                            style={{height:20, width:20, margin:4}}
-                                                        />
-                                                        <ThemedText type='defaultSemiBold'>{_player.wisdom_available}</ThemedText>
-                                                    </ThemedView>
-                                                </Pressable>
-                                            </View>
-                                        )
-                                    })}
-                                </View>
-                            )
-                            // if (props.hideCurrentPlayer && _player.id === props.player_id) {
-                            //     return null
-                            // }
-                        })}
-                    </ScrollView>
-                </ThemedView>
+                </>
             }
         </View>
     )
