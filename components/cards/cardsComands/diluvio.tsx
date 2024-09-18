90c2c37f966c5260506afaabafbd11c653f24513
marcosviana91 <marcos.viana.91@gmail.com>
2024-09-17 22:49:30 -0300

    fix - Dilúvio não precisa mirar em um oponente

--- components/cards/cardsComands/diluvio.tsx
+++ components/cards/cardsComands/diluvio.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '@/store';
@@ -15,22 +15,49 @@ import { getCardInListBySlug } from '@/hooks/useCards';
 export function OnInvoke() {
     const matchData = useSelector((state: RootReducer) => state.matchReducer.match_data)
     const player = useSelector((state: RootReducer) => state.matchReducer.player_data)
-    const [selectedPlayerId, setSelectedPlayerId] = useState<number>()
+    // const [selectedPlayerId, setSelectedPlayerId] = useState<number>()
     const player_in_match_data = usePlayerData(player?.id!)
 
-    const restauracao_de_fe_id = getCardInListBySlug('diluvio', player_in_match_data.card_prepare_camp)?.in_game_id
+    const diluvio_id = getCardInListBySlug('diluvio', player_in_match_data!.card_prepare_camp)?.in_game_id
 
     const WS = useAppWebSocket();
     const dispatch = useDispatch()
 
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log("Enviou Dilúvio " + diluvio_id)
+    //         WS.sendJsonMessage({
+    //             "data_type": "match_move",
+    //             "user_data": {
+    //                 "id": player?.id
+    //             },
+    //             "room_data": {
+    //                 "id": matchData?.id
+    //             },
+    //             "match_move": {
+    //                 "match_id": matchData?.id,
+    //                 "round_match": matchData?.round_match,
+    //                 "player_move": player?.id,
+    //                 "card_id": diluvio_id,// Restauração de Fé
+    //                 "move_type": "card_skill",
+    //             }
+    //         })
+    //     }, 500)
+    //     setTimeout(() => {
+    //         dispatch(setCurrentSkill(undefined))
+    //     }, 500)
+    // }, [player_in_match_data])
+
+    // return null
+
 
     return (
-        <ThemedModal title='Escolha um oponente para enviar o Dilúvio.' hideCloseButton closeModal={() => { }} >
-            <SelectEnemyIconsContainer matchData={matchData} player_id={player?.id} get_selected_player_id={setSelectedPlayerId} selected_player_id={selectedPlayerId} />
+        <ThemedModal title='Enviar o Dilúvio.' hideCloseButton closeModal={() => { }} >
+            {/* <SelectEnemyIconsContainer matchData={matchData} player_id={player?.id} get_selected_player_id={setSelectedPlayerId} selected_player_id={selectedPlayerId} /> */}
             <BasicButton
-                disabled={selectedPlayerId === undefined}
+                // disabled={selectedPlayerId === undefined}
                 onPress={() => {
-                    console.log(restauracao_de_fe_id + " inundou " + selectedPlayerId)
+                    // console.log(diluvio_id + " inundou " + selectedPlayerId)
                     WS.sendJsonMessage({
                         "data_type": "match_move",
                         "user_data": {
@@ -43,9 +70,9 @@ export function OnInvoke() {
                             "match_id": matchData?.id,
                             "round_match": matchData?.round_match,
                             "player_move": player?.id,
-                            "card_id": restauracao_de_fe_id,// Restauração de Fé
+                            "card_id": diluvio_id,// Dilúvio
                             "move_type": "card_skill",
-                            "player_target_id": selectedPlayerId,
+                            // "player_target_id": selectedPlayerId,
                         }
                     })
                     dispatch(setCurrentSkill(undefined))
