fee74c55689ff9fcb9ce9d2cfab275507f558fd2
marcosviana91 <marcos.viana.91@gmail.com>
2024-09-26 10:49:36 -0300

    fix - return undefined if not has match data

--- hooks/usePlayerData.ts
+++ hooks/usePlayerData.ts
@@ -55,16 +55,21 @@ export function useAvatar(
 }
 
 export function usePlayerData(player_id: number): PlayersInMatchApiProps | undefined {
-    const matchData = useSelector((state: RootReducer) => state.matchReducer.match_data)
-
-    let player_data = undefined
-    matchData!.players_in_match!.map((_team, _team_index) => {
-        _team.map(player => {
-            if (player.id === player_id) {
-                player_data = player;
-            }
+    try {
+        const matchData = useSelector((state: RootReducer) => state.matchReducer.match_data)
+    
+        let player_data = undefined
+        matchData!.players_in_match!.map((_team, _team_index) => {
+            _team.map(player => {
+                if (player.id === player_id) {
+                    player_data = player;
+                }
+            })
         })
-    })
-
-    return player_data
+    
+        return player_data
+        
+    } catch (error) {
+        return undefined
+    }
 }
