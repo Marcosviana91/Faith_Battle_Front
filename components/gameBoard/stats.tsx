916d59de27fe2115f5671e468e3e2f9f6d147410
marcosviana91 <marcos.viana.91@gmail.com>
2024-09-26 00:41:49 -0300

    feat - Stats screen on finish match

--- components/gameBoard/stats.tsx
+++ components/gameBoard/stats.tsx
@@ -1,8 +1,11 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ThemedText } from "../themed/ThemedText";
 import { ThemedView } from "../themed/ThemedView";
 import { RootReducer } from "@/store";
 import { AntDesign } from "@expo/vector-icons";
+import BasicButton from "../button/basic";
+import { leaveMatch } from "@/store/reducers/matchReducer";
+import { PlayerIcon } from "../player_user/playerIcon";
 
 function contaTempo(tempoInicial: number, tempoFinal: number) {
     const medidas = {
@@ -29,17 +32,40 @@ export default function Stats() {
     const matchData = useSelector((state: RootReducer) => state.matchReducer.match_data)!
     const start_time = new Date(matchData.start_match!).getTime();
     const end_time = new Date(matchData.end_match!).getTime()
-    
+
     const duracao_partida = contaTempo(start_time, end_time)
+    const dispatch = useDispatch()
 
     return (
         <ThemedView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
             <ThemedText type="title">A partida acabou!</ThemedText>
             <ThemedText type="link">ID: {matchData.id}</ThemedText>
-            <ThemedText>{duracao_partida} <AntDesign name="clockcircleo" size={18} /> - ROUND: {matchData.round_match}</ThemedText>
-            <ThemedView>
-
+            <ThemedText>{duracao_partida} <AntDesign name="clockcircleo" size={18} /> ROUND: {matchData.round_match}</ThemedText>
+            <ThemedView style={{gap:16, marginVertical:32, borderWidth:2, padding:4, borderRadius:8, width:'90%' }}>
+                <ThemedView style={{flexDirection:'row', gap:16}}>
+                    <ThemedText>JOGADOR:</ThemedText>
+                    <ThemedText>Pontos de Fé</ThemedText>
+                </ThemedView>
+                {matchData.players_in_match?.map(_team =>
+                (
+                    _team.map(_player => {
+                        return (
+                            <ThemedView style={{flexDirection:'row', gap:16}}>
+                                <PlayerIcon id={_player.id} type="mini" />
+                                <ThemedText>{_player.faith_points}</ThemedText>
+                            </ThemedView>
+                        )
+                    })
+                )
+                )}
             </ThemedView>
+            <BasicButton
+                onPress={() => {
+                    dispatch(leaveMatch())
+                }}
+            >
+                Sair
+            </BasicButton>
         </ThemedView>
     )
 }
