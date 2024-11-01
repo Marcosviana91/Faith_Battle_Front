45125087139e421ca9b472e3cf51ed6552523d3b
marcosviana91 <marcos.viana.91@gmail.com>
2024-11-01 18:32:31 -0300

    fix - style - SafeAreaProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Platform, View } from "react-native";
+import { Platform, View, } from "react-native";
 import { Provider } from "react-redux";
 import { Slot } from "expo-router";
 
@@ -7,19 +7,21 @@ import { ThemedView } from "@/components/themed/ThemedView";
 import { useScreenSizes } from "@/hooks/useScreenSizes";
 import { ConnectionStatus } from '@/components/connectionSpot';
 import { NotificationBox } from "@/components/notificationBox";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 
 export default function RootLayout() {
     const { width: windowWidth, height: windowHeight } = useScreenSizes();
     return (
-        <Provider store={store}>
-            {Platform.OS === 'android' && <ThemedView style={{ height: 50 }} lightColor="#fffc" darkColor="#000c" />}
-            <View style={{ height: windowHeight, maxWidth: windowWidth }}>
-                <ConnectionStatus />
-                <NotificationBox />
-                <Slot />
+        <SafeAreaProvider style={{ flex: 1 }}>
+            <Provider store={store}>
                 {Platform.OS === 'android' && <ThemedView style={{ height: 50 }} lightColor="#fffc" darkColor="#000c" />}
-            </View>
-        </Provider>
+                <View style={{ flex: 1, height: windowHeight, maxWidth: windowWidth , position:'relative'}}>
+                    <ConnectionStatus />
+                    <NotificationBox />
+                    <Slot />
+                </View>
+            </Provider>
+        </SafeAreaProvider>
     );
 }
